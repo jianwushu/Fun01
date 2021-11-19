a008a0a2d7d88469a0ce89f15a3de94ee3ff0af2
jianwushu <2574652740@qq.com>
2021-11-19 22:11:10 +0800

    修改

--- jd_fruit.js
+++ jd_fruit.js
@@ -1349,8 +1349,8 @@ function requireConfig() {
     console.log('开始获取配置文件\n')
     notify = $.isNode() ? require('./sendNotify') : '';
     //Node.js用户请在jdCookie.js处填写京东ck;
-    const jdCookieNode = $.isNode() ? require('./util/jdCookie.js') : '';
-    shareCodes = $.isNode() ? require('./util/getShareCodes.js') : '';
+    const jdCookieNode = $.isNode() ? require('jdCookie.js') : '';
+    shareCodes = $.isNode() ? require('getShareCodes.js') : '';
     const jdFruitShareCodes = $.isNode() ? shareCodes.getCodes("FRUITSHARECODES"): '';
     //IOS等用户直接用NobyDa的jd cookie
     if ($.isNode()) {
